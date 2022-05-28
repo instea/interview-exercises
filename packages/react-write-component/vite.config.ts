3178119f927749cb401c8e9fb859cc81f6dec5cf
jurajmatus <juraj.matus@instea.co>
2022-05-28 20:44:12 +0200

    Vite config change

--- packages/react-write-component/vite.config.ts
+++ packages/react-write-component/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import codesandbox from "@gsimone/codesandbox-vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import codesandbox from '@gsimone/codesandbox-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: process.env.HMR_PROTOCOL ? { protocol: process.env.HMR_PROTOCOL } : {},
+  },
   plugins: [react(), codesandbox()],
 });
