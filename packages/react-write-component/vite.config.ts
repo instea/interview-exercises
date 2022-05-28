b04a357aa0953061d45924c3dfac61f3d3e002d6
jurajmatus <juraj.matus@instea.co>
2022-05-28 21:17:44 +0200

    Final vite config fix

--- packages/react-write-component/vite.config.ts
+++ packages/react-write-component/vite.config.ts
@@ -1,27 +1,13 @@
 import react from '@vitejs/plugin-react';
-import { merge } from 'lodash';
-import { PluginOption, UserConfig, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-function codesandboxPlugin(): PluginOption {
-  if (typeof process.env.CODESANDBOX_HOST === 'undefined') {
-    return null;
-  }
-
-  return {
-    name: 'codesandbox-projects-plugin',
-    config: (config: UserConfig) =>
-      merge({}, config, {
-        server: {
-          hmr: {
-            clientPort: 443,
-            protocol: 'wss',
-          },
-        },
-      } as UserConfig),
-  };
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), codesandboxPlugin()],
+  plugins: [react()],
+  server: process.env.SANDBOX_URL
+    ? {
+        hmr: {
+          clientPort: 443,
+        },
+      }
+    : {},
 });
